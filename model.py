043ce7b35ec53aa6512bd49214aa9d2c3560042e
Mo'men AbdelRazek <runmoemen@gmail.com>
2017-03-02 22:15:57 +0200

    building the actor critic model

--- model.py
+++ model.py
@@ -1,48 +1,49 @@
 import tensorflow as tf
 
 
-class Actor():
+class Actor:
     """The actor class"""
 
-    def __init__(self, sess, config, num_actions, observation_shape):
+    def __init__(self, sess, num_actions, observation_shape, config):
         self._sess = sess
 
         self._state = tf.placeholder(dtype=tf.float32, shape=observation_shape, name='state')
         self._action = tf.placeholder(dtype=tf.int32, shape=[1], name='action')
         self._target = tf.placeholder(dtype=tf.float32, shape=[1], name='target')
 
-        self._output_layer = tf.contrib.layers.fully_connected(inputs=tf.expand_dims(self._state, 0), num_outputs=num_actions,
-                                                               activation_fn=None, weights_initializer=tf.zeros_initializer)
+        self._output_layer = tf.layers.dense(inputs=tf.expand_dims(self._state, 0), units=num_actions, kernel_initializer=tf.zeros_initializer())
         self._action_probs = tf.squeeze(tf.nn.softmax(self._output_layer))
         self._picked_action_prob = tf.gather(self._action_probs, self._action)
 
         self._loss = -tf.log(self._picked_action_prob) * self._target
 
         self._optimizer = tf.train.AdamOptimizer(learning_rate=config.learning_rate)
-        self._train_op = self.optimizer.minimize(self._lose)
+        self._train_op = self._optimizer.minimize(self._loss)
 
     def predict(self, s):
         return self._sess.run(self._action_probs, {self._state: s})
 
     def update(self, s, a, target):
-        self._sess.run(self._train_op, {self._state: s, self._action: a, self._target: target})
+        _, loss = self._sess.run(self._train_op, {self._state: s, self._action: a, self._target: target})
+        return loss
 
 
-class Critic():
+class Critic:
     """The critic class"""
 
-    def __init__(self, sess, config):
+    def __init__(self, sess, observation_shape, config):
         self._sess = sess
         self._config = config
         self._name = config.critic_name
+        self._observation_shape = observation_shape
         self._build_model()
 
     def _build_model(self):
         with tf.variable_scope(self._name):
-            self._state = None
-            self._target = None
+            self._state = tf.placeholder(dtype=tf.float32, shape=self._observation_shape, name='state')
+            self._target = tf.placeholder(dtype=tf.float32, shape=[1], name='target')
 
-            self._out = tf.layers.Dense()
+            self._out = tf.layers.dense(inputs=tf.expand_dims(self._state, 0), units=1, kernel_initializer=tf.zeros_initializer())
 
             self._value_estimate = tf.squeeze(self._out)
             self._loss = tf.squared_difference(self._out, self._target)
@@ -50,10 +51,9 @@ class Critic():
             self._optimizer = tf.train.AdamOptimizer(learning_rate=self._config.learning_rate)
             self._update_step = self._optimizer.minimize(self._loss)
 
+    def predict(self, s):
+        return self._sess.run(self._value_estimate, feed_dict={self._state: s})
 
-    def predict(self, state):
-        return self._sess.run(self._value_estimate, feed_dict={self._state: state})
-
-    def update(self, state, target):
-        _, loss = self._sess.run([self._update_step, self._loss], feed_dict={self._state: state, self._target: target})
+    def update(self, s, target):
+        _, loss = self._sess.run([self._update_step, self._loss], feed_dict={self._state: s, self._target: target})
         return loss
