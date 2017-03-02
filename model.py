3a11a5b4bfa4870148462d88525ad535dac49f78
3ammor <o.3ammor@gmail.com>
2017-03-02 17:30:30 +0200

    the model file architecture

---
+++ model.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+
+class BaseModel:
+    """The base class for the actor and critic estimators"""
+
+    def build_shared_model(self):
+        pass
+
+
+class Actor(BaseModel):
+    """The actor class"""
+
+    def __init__(self):
+        pass
+
+
+class Critic(BaseModel):
+    """The critic class"""
+
+    def __init__(self):
+        pass
